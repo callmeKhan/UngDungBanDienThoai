bf93a9c54bda95dc9d4338d4e4c21ca1ad2734bc
= <=>
2019-03-23 15:51:27 +0700

    develop commit

---
+++ app/src/main/java/com/nguyenminhtri/ungdungbandienthoai/view/dangky/ActivityDangKy.java
@@ -0,0 +1,15 @@
+package com.nguyenminhtri.ungdungbandienthoai.view.dangky;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.nguyenminhtri.ungdungbandienthoai.R;
+
+public class ActivityDangKy extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_ky);
+    }
+}
